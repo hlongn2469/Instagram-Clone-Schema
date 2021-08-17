33e7f978c6a4266e089f4b6df2d9375772bd107a
hlongn2469 <78957509+hlongn2469@users.noreply.github.com>
2021-08-17 14:46:42 -0700

    Update IGCloneChallenge.sql

--- IG-clone-db-schema/IGCloneChallenge.sql
+++ IG-clone-db-schema/IGCloneChallenge.sql
@@ -19,3 +19,18 @@ FROM users
 LEFT JOIN photos
     ON users.id = photos.user_id
 WHERE photos.id IS NULL;
+
+--4. Identify most popular photo (and user who created it)
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
