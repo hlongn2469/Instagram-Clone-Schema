d34144af8f0d644f30507e29b955e966717e0abe
hlongn2469 <78957509+hlongn2469@users.noreply.github.com>
2021-08-17 14:43:28 -0700

    Update IGCloneChallenge.sql

--- IG-clone-db-schema/IGCloneChallenge.sql
+++ IG-clone-db-schema/IGCloneChallenge.sql
@@ -1,1 +1,21 @@
+--1. Finding 5 oldest users
+SELECT * 
+FROM users
+ORDER BY created_at
+LIMIT 5;
 
+--2. Most Popular Registration Date
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
+
+--3. Identify Inactive Users (users with no photos)
+SELECT username
+FROM users
+LEFT JOIN photos
+    ON users.id = photos.user_id
+WHERE photos.id IS NULL;
