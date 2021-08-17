8567c74855b297a2485f622a46c7a0d79a0f5afe
hlongn2469 <78957509+hlongn2469@users.noreply.github.com>
2021-08-17 14:50:10 -0700

    Update IGCloneChallenge.sql

--- IG-clone-db-schema/IGCloneChallenge.sql
+++ IG-clone-db-schema/IGCloneChallenge.sql
@@ -34,3 +34,30 @@ INNER JOIN users
 GROUP BY photos.id
 ORDER BY total DESC
 LIMIT 1;
+
+-- 5. Calculate average number of photos per user
+
+SELECT (SELECT Count(*) 
+        FROM   photos) / (SELECT Count(*) 
+                          FROM   users) AS avg; 
+                          
+-- 6. Find the five most popular hashtags
+
+SELECT tags.tag_name, 
+       Count(*) AS total 
+FROM   photo_tags 
+       JOIN tags 
+         ON photo_tags.tag_id = tags.id 
+GROUP  BY tags.id 
+ORDER  BY total DESC 
+LIMIT  5; 
+
+-- 7. Finding the bots - the users who have liked every single photo
+SELECT username, 
+       Count(*) AS num_likes 
+FROM   users 
+       INNER JOIN likes 
+               ON users.id = likes.user_id 
+GROUP  BY likes.user_id 
+HAVING num_likes = (SELECT Count(*) 
+                    FROM   photos); 
